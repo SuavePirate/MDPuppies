b47c3881742a0807d6cf7b1c582e01cdf9877cab
Alex Dunn <alexander.dunn@outlook.com>
2017-05-24 12:10:41 -0400

    Initial project set up

---
+++ MDPuppies/MDPuppies/PuppyDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PuppyDetailViewController.swift
+//  MDPuppies
+//
+//  Created by Alex Dunn on 5/24/17.
+//  Copyright © 2017 Alex Dunn. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PuppyDetailViewController : UIViewController{
+    
+}
